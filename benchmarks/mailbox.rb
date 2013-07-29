416f0ee6fd8f59469417d9093cf97ae73d7e8cdf
Sławosz <slawosz@gmail.com>
2013-07-29 19:34:30 +0200

    add java mailbox to mailbox benchmark

--- benchmarks/mailbox.rb
+++ benchmarks/mailbox.rb
@@ -17,7 +17,26 @@ latch = Thread.new do
 end
 
 Benchmark.ips do |ips|
-  ips.report("messages") do |n|
+  ips.report("ruby messages") do |n|
+    latch_in << n
+    for i in 0..n; mailbox << :message; end
+    latch_out.pop
+  end
+end
+
+mailbox = Celluloid::JavaMailbox.new
+latch_in, latch_out = Queue.new, Queue.new
+
+latch = Thread.new do
+  while true
+    n = latch_in.pop
+    for i in 0..n; mailbox.receive; end
+    latch_out << :done
+  end
+end
+
+Benchmark.ips do |ips|
+  ips.report("java messages") do |n|
     latch_in << n
     for i in 0..n; mailbox << :message; end
     latch_out.pop
